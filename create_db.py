c83516196f9d6a86c54c64a828247e5ce8ff53f2
Alana Gabriele Amorim Silva <alanagabriele43@gmail.com>
2025-10-08 13:50:31 -0300

    feat: adicionando portal agora no vale em create_db

--- create_db.py
+++ create_db.py
@@ -2,14 +2,7 @@ from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from models import (
-    Advertisement,
-    Portal,
-    Entry,
-    QueueStatus,
-    URLQueue,
-)
-
+from models import Advertisement, Entry, Portal, QueueStatus, URLQueue
 
 if __name__ == "__main__":
     engine = create_engine(config("DATABASE_URL"))
@@ -42,6 +35,7 @@ if __name__ == "__main__":
         ("Gazeta do Povo", "https://www.gazetadopovo.com.br/", "gazetadopovo"),
         ("Mais Goiás", "https://www.maisgoias.com.br/", "maisgoias"),
         ("Aliados Brasil", "https://www.aliadosbrasiloficial.com.br/", "aliadosbrasil"),
+        ("Agora no Vale", "https://www.agoranovale.com.br/", "agoranovale"),
     ]
     portals_to_add = []
     for portal in portals:
