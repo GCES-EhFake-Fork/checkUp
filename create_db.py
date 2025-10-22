9766c9b2f0d8180bdb848706bf8eea26c7ff6fa4
Eduardo Ferreira <almeifaferreira@gmail.com>
2025-10-22 12:52:25 -0300

    feat(db): registrar Jornal de Brasília no create_db para criação automática

--- create_db.py
+++ create_db.py
@@ -42,6 +42,7 @@ if __name__ == "__main__":
         ("Gazeta do Povo", "https://www.gazetadopovo.com.br/", "gazetadopovo"),
         ("Mais Goiás", "https://www.maisgoias.com.br/", "maisgoias"),
         ("Aliados Brasil", "https://www.aliadosbrasiloficial.com.br/", "aliadosbrasil"),
+        ("Jornal de Brasília", "https://www.jornaldebrasilia.com.br/", "jornaldebrasilia"),
     ]
     portals_to_add = []
     for portal in portals:
